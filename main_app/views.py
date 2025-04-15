7d856dbfb909e09cd7f73e5f9afa33ae1f345013
Michael McBride <michaelmcbride113@gmail.com>
2025-04-14 22:58:31 -0400

    Added hike list styling

--- main_app/views.py
+++ main_app/views.py
@@ -3,9 +3,29 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+# views.py
+
+class Hike:
+    def __init__(self, name, location, difficulty, date, description):
+        self.name = name
+        self.location = location
+        self.difficulty = difficulty
+        self.date = date
+        self.description = description
+
+# Create a list of Cat instances
+hikes = [
+    Hike('Mt. Beacon', 'Beacon, NY', 'Medium', '4/5/2025', 'Fun hike, tough at the beginning but gets easier'),
+    Hike('Breakneck Ridge', 'Cold Spring, NY', 'Medium', '4/6/2025', 'Fun hike, lots of scrambling'),
+    Hike('Lake Minewaska', 'New Paltz, NY', 'Easy', '4/7/2025', 'Easy loop trail around a lake')
+]
+
+
 def home(request):
     return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 def about(request):
     return render(request, 'about.html')
 
+def hike_index(request):
+    return render(request, 'hikes/index.html', { 'hikes': hikes })
