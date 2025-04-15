a1a980e4bb04ce80d4c45dc9b57277337fdf9114
Michael McBride <michaelmcbride113@gmail.com>
2025-04-15 07:40:29 -0400

    Added update and delete functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('hikes/', views.hike_index, name='hike-index'),
     path('hikes/<int:hike_id>/', views.hike_detail, name='hike-detail'),
     path('hikes/create/', views.HikeCreate.as_view(), name='hike-create'),
+    path('hikes/<int:pk>/update/', views.HikeUpdate.as_view(), name='hike-update'),
+    path('hikes/<int:pk>/delete/', views.HikeDelete.as_view(), name='hike-delete'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,19 @@
 
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Hike
 
 class HikeCreate(CreateView):
     model = Hike
     fields = '__all__'
-    # success_url = '/hikes/'
 
-# class Hike:
-#     def __init__(self, name, location, difficulty, date, description):
-#         self.name = name
-#         self.location = location
-#         self.difficulty = difficulty
-#         self.date = date
-#         self.description = description
+class HikeUpdate(UpdateView):
+    model = Hike
+    fields = '__all__'
 
-# hikes = [
-#     Hike('Mt. Beacon', 'Beacon, NY', 'Medium', '4/5/2025', 'Fun hike, tough at the beginning but gets easier'),
-#     Hike('Breakneck Ridge', 'Cold Spring, NY', 'Medium', '4/6/2025', 'Fun hike, lots of scrambling'),
-#     Hike('Lake Minewaska', 'New Paltz, NY', 'Easy', '4/7/2025', 'Easy loop trail around a lake')
-# ]
+class HikeDelete(DeleteView):
+    model = Hike
+    success_url = '/hikes/'
 
 def home(request):
     return render(request, 'home.html')
