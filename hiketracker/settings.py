f858e181b36bdb881e835834efd68c166bf6dd02
Michael McBride <michaelmcbride113@gmail.com>
2025-04-14 23:23:21 -0400

    Added psql database

--- hiketracker/settings.py
+++ hiketracker/settings.py
@@ -75,10 +75,11 @@ WSGI_APPLICATION = 'hiketracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hiketracker',
     }
 }
+
 
 
 # Password validation
